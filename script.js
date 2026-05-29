c61ddd1b72c27884fd7339940ab0bae96c142fa5
Daniel Nowak <danieln311@gmail.com>
2026-05-29 09:02:48 +0000

    Change html tags for digits and operators, add event listeners to operator buttons, further work on operate function, add seperate function for operator when clicked

--- script.js
+++ script.js
@@ -3,63 +3,71 @@ let numberTwo = "";
 let operator = ""
 const calculatorButtons = document.querySelectorAll(".calculatorButtons")
 const digitButtons = document.querySelectorAll(".digitButtons")
+const operatorButtons = document.querySelectorAll(".operatorButtons")
 const display = document.querySelector("#display")
+const clearButton = document.querySelector("#acButton")
+const equalsButton = document.querySelector("#equalsButton")
+
+clearButton.addEventListener("click", clearAll)
+function clearAll() {
+    display.value = "";
+    numberOne = "";
+    numberTwo = "";
+    operator = "";
+}
 
 let multiply = function(numberOne, numberTwo) {
-    console.log(numberOne * numberTwo)
+    numberOne * numberTwo
 }
 let divide = function(numberOne, numberTwo) {
-    console.log(Number(numberOne) / Number(numberTwo))
+    Number(numberOne) / Number(numberTwo)
 }
 let add = function(numberOne, numberTwo) {
-    console.log(numberOne + numberTwo)
+    numberOne + numberTwo
 }
 let subtract = function(numberOne, numberTwo) {
-    console.log(numberOne - numberTwo)
+    numberOne - numberTwo
 }
 
-const operate = function(numberOne, numberTwo, operator) {
+function operate (numberOne, numberTwo, operator) {
     switch (operator) {
-        case "*":
-            multiply(numberOne, numberTwo)
+        case "X":
+            display.value = multiply(numberOne, numberTwo)
             break;
         case "/":
-            divide(numberOne, numberTwo)
+            display.value = divide(numberOne, numberTwo)
             break;
         case "+":
-            add(numberOne, numberTwo)
+            display.value = add(numberOne, numberTwo)
             break;
         case "-":
-            subtract(numberOne, numberTwo)
+            display.value = subtract(numberOne, numberTwo)
     }
 }
+equalsButton.addEventListener("click", operate)
 
 digitButtons.forEach((button) => {
-    button.addEventListener("click", updateValues)
+    button.addEventListener("click", updateNumbers)
 })
-calculatorButtons.forEach((button) => {
-    button.addEventListener("click", updateValues)
+operatorButtons.forEach((button) => {
+    button.addEventListener("click", updateOperator)
 })
+function updateOperator(e) {
+    operator = e.target.innerText
+    display.value = e.target.innerText;
+}
 
-function updateValues(e) {
-    let currentNum = "";
-
-    if (numberOne === "") {
+function updateNumbers(e) {
+    if (numberOne == "") {
         numberOne = e.target.innerText;
         display.value += e.target.innerText;
         console.log(e.target.innerText)
     }
-    else if (operator === "") {
-        operator = e.target.innerText;
-        display.value += e.target.innerText;
-        console.log(e.target.innerText)
+    else if (operator == "") {
+        updateOperator();
     }
-    else {
+    else if(numberTwo == "") {
         numberTwo = e.target.innerText;
-        display.value += e.target.innerText;
-        console.log(e.target.innerText)
+        display.value = e.target.innerText;
     }
-    console.log("numberOne is:" + numberOne);
-    console.log("operator is:" + operator);
-    console.log(operator);
 }
